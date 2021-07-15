c06fe4479746f6ac3e44caa63284605d085216e8
ksinger12 <kylesinger@rogers.com>
2021-07-15 00:06:05 -0400

    setting up token

--- src/butter-client.js
+++ src/butter-client.js
@@ -1,5 +1,7 @@
 import Butter from 'buttercms';
 
-const butter = Butter('f44b610b408db16b732c5c5cd260665de55df2eb');
+const API_TOKEN = "";
+
+const butter = Butter(API_TOKEN);
 
 export default butter;
